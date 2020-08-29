6c8978bb36f0b8f1e4e8c305c02b9b0c3a37c734
sonulen <sonulen@gmail.com>
2020-08-30 00:14:24 +0300

    update: Добавил TabLayout + ViewPage + Fabs

--- app/src/main/java/com/example/devlife/MainActivity.kt
+++ app/src/main/java/com/example/devlife/MainActivity.kt
@@ -1,18 +1,61 @@
 package com.example.devlife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.devlife.ui.main.MainFragment
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
+    private var viewPager: ViewPager? = null
+    private var tabLayout: TabLayout? = null
+    private val POSITION = "POSITION"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        viewPager = findViewById(R.id.viewpager) as ViewPager
+        tabLayout = findViewById(R.id.tablayout) as TabLayout
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+            viewPager?.let {
+                it.offscreenPageLimit = 3
+                setupViewPager(it)
+            }
+            tabLayout?.let {
+                it.setupWithViewPager(viewPager)
+            }
+
         }
     }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(MainFragment.newInstance(), getString(R.string.first_category))
+        adapter.addFragment(MainFragment.newInstance(), getString(R.string.second_category))
+        adapter.addFragment(MainFragment.newInstance(), getString(R.string.third_category))
+        viewPager.adapter = adapter
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        tabLayout?.let {
+            outState.putInt(POSITION, it.getSelectedTabPosition())
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val tabIndex = savedInstanceState.getInt(POSITION)
+        viewPager?.let {
+            it.setCurrentItem(tabIndex);
+        }
+        tabLayout?.let {
+            it.setScrollPosition(tabIndex,0f,true);
+        }
+    }
+
 }
